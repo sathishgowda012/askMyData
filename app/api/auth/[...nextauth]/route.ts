bda951d12058dcc297a4b606632a23512e63ff29
Sathish <sathish@Sathishs-MacBook-Pro.local>
2024-11-06 18:47:21 +0530

    added auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,10 @@
-import NextAuth from "next-auth"
+// import { handlers } from "@/auth" // Referring to the auth.ts we just created
+import Google from "next-auth/providers/google"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+export const { GET, POST } = handlers
+import { handlers } from '../../../auth';
+
+
+
+
+// app/auth.ts
